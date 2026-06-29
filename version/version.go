8ad792dac121bc0ab8355cb7740601e9f381df5c
matbur <mateusz@burniak.pl>
2026-06-29 21:33:19 +0200

    Update Cloud Build configuration to use SHORT_SHA for build arguments and clarify Commit variable documentation

--- version/version.go
+++ version/version.go
@@ -1,4 +1,10 @@
 package version
 
-// Commit is the short git SHA, set at build time via -ldflags.
+// Commit is the git SHA, set at build time via -ldflags.
 var Commit = "dev"
+
+func init() {
+	if len(Commit) > 7 {
+		Commit = Commit[:7]
+	}
+}
